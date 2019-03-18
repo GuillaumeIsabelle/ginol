46c09a6c20f097d7436a0482c87543c040ce0454
Guillaume Isabelle <jgi@jgwill.com>
2019-03-18 17:35:43 -0400

    fix

--- ginol.js
+++ ginol.js
@@ -1,5 +1,14 @@
-var gulp = require("gulp"),
-	notify = require("gulp-notify");
+#!/usr/bin/env node
+/** Guillaume Isabelle GINOL
+ * Vision: Simple notification are received and a simple command generates them
+
+ * Current Reality: Gulp notify mastery
+ */
+
+
+var gulp = require("gulp");
+
+var	notify = require("gulp-notify");
 
 var args = process.argv.slice(2);
 
